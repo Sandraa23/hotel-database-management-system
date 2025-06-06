f0a9dcb5f71d4bed902736481e5c70bdcdde5959
Sandra <saandra.j2306@gmail.com>
2025-06-06 20:19:20 +0530

    Update dbms.py

--- dbms.py
+++ dbms.py
@@ -9,7 +9,7 @@ def get_db_connection():
     conn = mysql.connector.connect(
         host='localhost',
         user='root',
-        password='SAandra@03',
+        password='12345',
         database='dbproj'
     )
     return conn
